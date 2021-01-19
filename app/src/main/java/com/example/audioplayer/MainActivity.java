2068fb503dd9cfe1604d578fb79da70dd57c5aa5
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-01-19 17:22:39 +0200

    Create model(MusicFiles => getAllAudio) , Permission

--- app/src/main/java/com/example/audioplayer/MainActivity.java
+++ app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -1,28 +1,67 @@
 package com.example.audioplayer;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
-
-import android.os.Bundle;
 
 import com.example.audioplayer.adapter.ViewPagerAdapter;
 import com.example.audioplayer.fragments.AlbumFragment;
 import com.example.audioplayer.fragments.SongFragment;
+import com.example.audioplayer.model.MusicFiles;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE =1;
+    ArrayList<MusicFiles> musicFiles;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        permission();
+        
         initView();
+        
+    }
+
+    private void permission() {
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+        }
+        else {
+            Toast.makeText(this, "Разрешение получено!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Разрешение получено!", Toast.LENGTH_SHORT).show();
+
+            }
+            else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+                musicFiles = getAllAudio(this);
+            }
+        }
     }
 
     private void initView() {
@@ -35,5 +74,36 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
+    public static ArrayList<MusicFiles> getAllAudio (Context context){
+        ArrayList<MusicFiles> audioList = new ArrayList<>();
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.ARTIST
 
+        };
+
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+        if(cursor != null){
+            while (cursor.moveToNext()){
+                String album = cursor.getString(0);
+                String title = cursor.getString(1);
+                String duration = cursor.getString(2);
+                String path = cursor.getString(3);
+                String artist = cursor.getString(4);
+
+                MusicFiles musicFiles = new MusicFiles(path, title ,artist, album, duration );
+
+                //log
+                Log.e("PATH:"+ path, "Albums:" + album);
+                audioList.add(musicFiles);
+            }
+            cursor.close();
+        }
+        return audioList;
+
+    }
 }
