4ae8587a4ce25e15b926d073e17005a5812f2443
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2021-03-09 16:01:35 +0200

    AlbumsDetailsAdapter

---
+++ app/src/main/java/com/example/audioplayer/adapter/AlbumDetailsAdapter.java
@@ -0,0 +1,73 @@
+package com.example.audioplayer.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaMetadataRetriever;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.audioplayer.R;
+import com.example.audioplayer.model.MusicFiles;
+
+import java.util.ArrayList;
+
+public class AlbumDetailsAdapter extends RecyclerView.Adapter<AlbumDetailsAdapter.MyHolder> {
+
+    private Context mContext;
+    private ArrayList<MusicFiles> albumFiles;
+    View view;
+    public AlbumDetailsAdapter(Context mContext, ArrayList<MusicFiles> albumFiles) {
+        this.mContext = mContext;
+        this.albumFiles = albumFiles;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        view= LayoutInflater.from(mContext).inflate(R.layout.menu_items, parent,false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        holder.album_name.setText(albumFiles.get(position).getTitle());
+        byte [] image = getAlbumArt(albumFiles.get(position).getPath());
+        if (image != null){
+            Glide.with(mContext).asBitmap().load(image).into(holder.album_image);
+        }
+        else {
+            Glide.with(mContext).load(R.drawable.eminem_kamikaze).into(holder.album_image);       //Если нет то берем картинку
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return albumFiles.size();
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder{
+        ImageView album_image;
+        TextView album_name;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            album_image=itemView.findViewById(R.id.music_img);
+            album_name= itemView.findViewById(R.id.music_file_name);
+        }
+    }
+    private byte [] getAlbumArt (String uri){
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte [] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
+    }
+}
--- app/src/main/java/com/example/audioplayer/adapter/AlbumsDetails.java
+++ app/src/main/java/com/example/audioplayer/adapter/AlbumsDetails.java
@@ -1,6 +1,7 @@
 package com.example.audioplayer.adapter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.media.MediaMetadataRetriever;
@@ -21,7 +22,7 @@ public class AlbumsDetails extends AppCompatActivity {
     ImageView albumPhoto;
     String albumName;
     ArrayList<MusicFiles> albumsSongs = new ArrayList<>();
-
+    AlbumDetailsAdapter albumDetailsAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,26 +30,37 @@ public class AlbumsDetails extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         albumPhoto = findViewById(R.id.albumPhoto);
         albumName = getIntent().getStringExtra("albumName");
-        int j = 0;
-        for (int i = 0; i < musicFiles.size(); i++) {
-            if (albumName.equals(musicFiles.get(i).getAlbum())) {
+        int j=0;
+        for (int i=0; i< musicFiles.size(); i++){
+            if(albumName.equals(musicFiles.get(i).getAlbum())){
                 albumsSongs.add(j, musicFiles.get(i));
                 j++;
             }
         }
-        byte[] image = getAlbumArt(albumsSongs.get(0).getPath());
+        byte [] image = getAlbumArt(albumsSongs.get(0).getPath());
         if (image != null){
             Glide.with(this).load(image).into(albumPhoto);
         }
         else {
-            Glide.with(this).load(R.drawable.eminem_kamikaze).into(albumPhoto);
+            Glide.with(this).load(R.drawable.eminem_kamikaze).into(albumPhoto);       //Если нет то берем картинку
         }
     }
 
-    private byte[] getAlbumArt(String uri) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!(albumsSongs.size()<1)){
+            albumDetailsAdapter=new AlbumDetailsAdapter(this,albumsSongs);
+            recyclerView.setAdapter(albumDetailsAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                    RecyclerView.VERTICAL,false));
+        }
+    }
+
+    private byte [] getAlbumArt (String uri){
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(uri);
-        byte[] art = retriever.getEmbeddedPicture();
+        byte [] art = retriever.getEmbeddedPicture();
         retriever.release();
         return art;
     }
